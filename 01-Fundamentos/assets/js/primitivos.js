9fc6a9c0c48e1778c000967f42275b7ab2dbdd45
Javier Briones <javier.brms@gmail.com>
2022-01-24 19:59:41 -0300

    ejemplos de datos primitivos

--- 01-Fundamentos/assets/js/primitivos.js
+++ 01-Fundamentos/assets/js/primitivos.js
@@ -4,3 +4,33 @@ console.log( nombre );
 
 nombre = 'Ben Parker';
 console.log(nombre);
+
+nombre = "Tia May";
+nombre = `Tia May`;
+
+console.log(typeof nombre);
+
+nombre = 123;
+console.log(typeof nombre);
+
+let esMarvel = true;
+
+console.log(typeof esMarvel);
+
+let edad = 33;
+console.log(typeof edad);
+
+edad = 33.001;
+console.log(typeof edad);
+
+let superPoder;
+console.log(typeof superPoder);
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log (symbol1 === symbol2);
