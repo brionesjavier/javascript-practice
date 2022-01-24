204c114b00311ac46e19f7497133fd1065121429
Javier Briones <javier.brms@gmail.com>
2022-01-24 20:32:27 -0300

    ejemplo palabras reservadas

---
+++ 01-Fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,22 @@
+
+//let cons =123; no se puede ocupar por que es una palabra reservada
+let constante = 123;
+
+//const
+//try
+//while
+//for
+//await
+
+// permitidas
+// los nombres tienen que ser significativos 
+let _nombre =123;
+let objeto$ = 234;
+let objetos123 = 123;
+let precio_99 = 123;
+
+//lowerCameCase     ejemplo: ejemploDeLowerCamelCase para variables
+//upperCameCase     ejemplo: EjemploDeUpperCamelCase para clases
+class JuegoAnio{
+
+}
