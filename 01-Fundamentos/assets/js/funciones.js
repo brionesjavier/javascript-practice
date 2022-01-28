24dc506b92208f99a7b1932243c0540bd7e05514
Javier Briones <javier.brms@gmail.com>
2022-01-27 22:29:07 -0300

    ejemplos funciones y flechas parte 2

--- 01-Fundamentos/assets/js/funciones.js
+++ 01-Fundamentos/assets/js/funciones.js
@@ -17,9 +17,38 @@
  const saludarFlecha2 = (nombre) =>{
     console.log( 'Hola Flecha '+nombre );
 }
- //let nombre = prompt('¿cuál es tu nombre?','sin nombre');
- saludar( 'Fernando', 40, true, 'Costa Rica' );
- saludar2( 'Tania ' );
+//  let nombre = prompt('¿cuál es tu nombre?','sin nombre');
+//  saludar( 'Fernando', 40, true, 'Costa Rica' );
+//  saludar2( 'Tania ' );
 
- saludarFlecha();
- saludarFlecha2( 'Rodrigo' );
+//  saludarFlecha();
+//  saludarFlecha2( 'Rodrigo' );
+
+
+ //MAS DETALLES 
+
+function sumar( a, b){
+    return a + b;
+}
+
+// console.log( sumar( 1, 2) );
+
+// const sumar2 = ( a, b) => { //forma larga cunado son varias lineas
+//     return a + b;
+// }
+
+const sumar2 = ( a, b) => a + b;
+
+console.log( sumar2( 2, 2));
+
+
+function getAleatorio(){
+    return Math.random();
+}
+
+console.log( getAleatorio() );
+
+//ejercicio crear una funcion de flecha, que no tenga llaves {}
+
+const getAleatorio2 = () => Math.random();
+console.log( getAleatorio2() );
