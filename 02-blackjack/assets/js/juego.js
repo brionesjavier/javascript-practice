770239bc0651a075417c5d502f1b209f1a5f4b19
Javier Briones <javier.brms@gmail.com>
2022-02-01 18:39:40 -0300

    Creada baraja de cartas

---
+++ 02-blackjack/assets/js/juego.js
@@ -0,0 +1,33 @@
+/**
+ * 2C = Two of Clubs    (Tréboles) 
+ * 2D = Two of Diamonds (Diamantes)
+ * 2H = Two of Hearts   (Corazones)
+ * 2S = Two of Spades   (Espadas)
+ */
+
+
+let deck = [];
+const tipos =['C','D','H','S'];
+const especiales =['A','J','Q','K'];
+
+const crearDeck = () => {
+
+    for(let i = 2; i <= 10; i++){
+        for(let tipo of tipos){
+            deck.push( i + tipo );
+        }
+    }
+    for( let tipo of tipos ){
+        for( let especial of especiales ){
+            deck.push( especial + tipo );
+        }
+    }
+
+    //console.log( deck );
+    deck = _.shuffle( deck );
+    console.log( deck );
+    return deck;
+
+}
+
+crearDeck();
