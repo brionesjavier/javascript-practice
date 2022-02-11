77e991820d5c71c1d06f1332d04b0fcdc90e320b
Javier Briones <javier.brms@gmail.com>
2022-02-11 19:35:52 -0300

    propiedades, gets y metodos estaticos

--- 01-Fundamentos/assets/js/classes/class.js
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -3,9 +3,21 @@ class Persona{
     //use strict viene por defecto en la clase
     //inicializando
 
-    nombre   = '' ;
-    codigo   = '' ;
-    frase    = '' ;
+    //propiedades estaticas
+    static _conteo = 0;
+
+    static get getConteo(){
+        return Persona._conteo + ' instancias'
+    }
+    // metodo estatico
+    static mensaje(){
+        console.log(this.nombre);
+        console.log('Este es un método estatico');
+    }
+
+    nombre  = '';
+    codigo  = '';
+    frase   = '';
     comida  = '';
 
     constructor( nombre = 'Sin nombre', codigo = 'Sin codigo', frase = 'Sin frase'){
@@ -15,6 +27,7 @@ class Persona{
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+        Persona._conteo++;
     }
 
     set setComidaFavorita( comida ){
@@ -40,16 +53,31 @@ class Persona{
 const spiderman = new Persona('Peter Parker','Spiderman','Soy tu amigable vecino Spiderman');
 const ironman = new Persona('Tony Stark','Ironman','Yo soy Ironman');
 
-console.log( spiderman );
-console.log( ironman );
+// console.log( spiderman );
+// console.log( ironman );
 
 //spiderman.quienSoy();
 //ironman.quienSoy();
-ironman.miFrase();
+// ironman.miFrase();
 
 //set y get
 
 spiderman.setComidaFavorita = 'El pie de cereza de la tia Mey';
 
 //spiderman.nemesis = 'Duende verde'
-console.log( spiderman.getComidaFavorita );
+// console.log( spiderman.getComidaFavorita );
+
+
+//estatico
+//Persona._conteo = 2;
+
+console.log('Conteo estatico',Persona._conteo );
+
+console.log(Persona.getConteo);
+
+Persona.mensaje();
+
+Persona.propiedadExterna = 'Hola Mundo';
+
+console.log(Persona.propiedadExterna );
+console.log( Persona );
