e08473fb4c2715f31dcf876aa81e92154c5af141
Javier Briones <javier.brms@gmail.com>
2022-02-10 21:57:05 -0300

    set y get

--- 01-Fundamentos/assets/js/classes/class.js
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -1,9 +1,12 @@
 
 class Persona{
     //use strict viene por defecto en la clase
-    // nombre  = "" ;
-    // codigo  = "" ;
-    // frase   = "" ;
+    //inicializando
+
+    nombre   = '' ;
+    codigo   = '' ;
+    frase    = '' ;
+    comida  = '';
 
     constructor( nombre = 'Sin nombre', codigo = 'Sin codigo', frase = 'Sin frase'){
 
@@ -13,6 +16,17 @@ class Persona{
         this.codigo = codigo;
         this.frase = frase;
     }
+
+    set setComidaFavorita( comida ){
+        this.comida = comida;
+    }
+
+    get getComidaFavorita(){
+        return `La comida favorita de ${ this.nombre } es ${ this.comida }`;
+    }
+
+
+
     quienSoy(){
         console.log(`soy ${ this.nombre } y mi identidad es ${ this.codigo }`);
     }
@@ -32,3 +46,10 @@ console.log( ironman );
 //spiderman.quienSoy();
 //ironman.quienSoy();
 ironman.miFrase();
+
+//set y get
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tia Mey';
+
+//spiderman.nemesis = 'Duende verde'
+console.log( spiderman.getComidaFavorita );
