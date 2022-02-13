65a8b9159fa551e011c68b110b9445f00cc0c41c
Javier Briones <javier.brms@gmail.com>
2022-02-13 00:57:46 -0300

    actualizado archivo de configuracion webpack

--- 03-webpack-inicial/webpack.config.js
+++ 03-webpack-inicial/webpack.config.js
@@ -1,17 +1,31 @@
 
-module.exports ={
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
 
     mode: 'development',
-    module:{
-        rules:[
-            {
-                test: /\.thml$/,
-                optiond:{
-                    attributes: false,
-                }
-            }
-        ]
-    }
 
+    module: {
+      rules: [
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
 
-}
+          options:{
+              sources: false,
+              minimize: false,
+          }
+        },
+      ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({  // Also generate a test.html
+            
+            template: './src/index.html',
+            filename: './index.html'
+          
+        }),
+   ]
+
+  };
