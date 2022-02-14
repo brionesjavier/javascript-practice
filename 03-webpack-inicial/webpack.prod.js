977da132da31f4d74be5032ad599af6ebd8707d2
Javier Briones <javier.brms@gmail.com>
2022-02-14 00:07:35 -0300

    webpack - Production Mode

---
+++ 03-webpack-inicial/webpack.prod.js
@@ -0,0 +1,88 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+
+    mode: 'production',
+    
+    optimization: {
+
+      minimizer: [
+        
+        new CssMinimizerWebpackPlugin(),
+        
+      ]
+
+    },
+    output:{
+      filename: 'main.[contenthash].js'
+    },
+
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          exclude: /style\.css$/i,
+          use: [
+             //el orden que se llama los package inporta
+            "style-loader", 
+            "css-loader"
+          ],
+        },
+
+        {
+          test: /style\.css$/i,//error aqui
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            //'sass-loader'
+          ],
+        },
+
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+
+          options:{
+              sources: false,
+              minimize: false,
+          }
+        },
+
+      ],
+    },
+    
+
+    plugins: [
+
+             
+
+        new HtmlWebpackPlugin({  // Also generate a test.html
+            
+            template: './src/index.html',
+            filename: './index.[contenthash].html'
+          
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            //filename: '[name].css',
+            ignoreOrder: false
+        }),
+
+        new CopyWebpackPlugin({
+          
+          patterns: [
+
+            { from: 'src/assets', to: 'assets/'},
+          ]
+
+        }), 
+   ]
+
+  };
