df197727bc888d38a2c400828e9aead0af1924db
Javier Briones <javier.brms@gmail.com>
2022-02-15 20:01:50 -0300

    probando git

--- 03-webpack-inicial/webpack.prod.js
+++ 03-webpack-inicial/webpack.prod.js
@@ -50,8 +50,13 @@ module.exports = {
             exclude: /node_modules/,
             use: [
               "babel-loader",
-    
             ]
+              //   use: {
+              //     loader: "babel-loader",
+              //     options: {
+              //       presets: ['@babel/preset-env']
+              //     }
+              //   }
 
         },
 
