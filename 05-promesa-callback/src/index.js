c8634fc769ac122ba946eb89c16061a9abb26862
javier briones cañas <javier.brms@gmail.com>
2022-03-20 22:51:23 -0300

    identacion

--- 05-promesa-callback/src/index.js
+++ 05-promesa-callback/src/index.js
@@ -12,18 +12,17 @@ const heroeId3 = 'spider';
 buscarHeroe(heroeId1,( err,heroe1 )=>{
 
     if ( err ) {return console.error( err );}
-    buscarHeroe(heroeId2, (err, heroe2)=>{
-        if ( err ) {return console.error( err );}
-        buscarHeroe(heroeId3, (err, heroe3)=>{
+        buscarHeroe(heroeId2, (err, heroe2)=>{
             if ( err ) {return console.error( err );}
-    
-                console.log(`Enviando a ${ heroe1.nombre } , ${ heroe2.nombre } y ${ heroe3.nombre }a la mision`);
-    
-        })
+                buscarHeroe(heroeId3, (err, heroe3)=>{
+                    if ( err ) {return console.error( err );}
 
-    })
-       
-      
+                        console.log(`Enviando a ${ heroe1.nombre } , ${ heroe2.nombre } y ${ heroe3.nombre }a la mision`);
+
+                })
+
+        })
+        
 });
 
 console.log('Fin del Programa ');
